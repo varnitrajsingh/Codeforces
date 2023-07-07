b35838030b427f13198f25a53026c743cbe462c9
Varnit Raj Singh <varnitrajsingh@gmail.com>
2023-07-07 23:54:12 +0530

    new code

--- Bitpp.java
+++ Bitpp.java
@@ -1,5 +1,22 @@
+import java.util.*;
 public class Bitpp {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int n=0;
+        while(t!=0){
+            String str = sc.next();
+            if(str.equals("++X") || (str.equals("X++"))){
+                n++;
+            }
+            else {
+                n--;
+            }
+            t--;
+            
+        }
+        System.out.println(n);
+        sc.close();
+
     }
 }
