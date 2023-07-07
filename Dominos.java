0b638f98dd3511ab2ad56743eb82f0b5ebdcc4a6
Varnit Raj Singh <varnitrajsingh@gmail.com>
2023-07-07 22:58:29 +0530

    First commit

---
+++ Dominos.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Dominos{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int result;
+        result = (m*n)/2;
+        System.out.println(result);
+        sc.close();
+    }
+}
